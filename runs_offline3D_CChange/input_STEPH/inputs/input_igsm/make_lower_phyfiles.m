f704b86ff7d72b5590a84ddc90bd49ddc8ca638d
bsauterey <bsauterey@gmail.com>
2025-07-16 15:28:33 +0200

    Remove CC

--- runs_offline3D_CChange/input_STEPH/inputs/input_igsm/make_lower_phyfiles.m
+++
@@ -1,8 +0,0 @@
-fid=fopen('phy78_lev01_ann-3d.bin','r','b');
-phyto=fread(fid,'float32'); fclose(fid);
-phyto=reshape(phyto,144,90,22);
-
-newphyto=phyto*0.1;
-
-fid=fopen('phy_low_ann-3d.bin','w','b');
-fwrite(fid,newphyto,'float32'); fclose(fid);
